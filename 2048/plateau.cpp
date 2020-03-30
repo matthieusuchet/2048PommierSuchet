9a9de7967674ca618f45920f5ea80d2f178aa0ec
Matthieu Suchet <msuchet@hotmail.fr>
2020-03-30 10:31:15 +0200

    fichier plateau.cpp initial

--- 2048/plateau.cpp
+++ 2048/plateau.cpp
@@ -1,6 +1,14 @@
 #include "plateau.h"
+#include <iostream>
+using namespace std;
 
-Plateau::Plateau()
+Plateau::Plateau(int n)
 {
-
+    taille = n;
+    table = new int*[taille]; // création du tableau
+    for (int i=0; i<n; i++) {
+        table[i] = new int[n];
+    }
+    reset_table(); // initialise à 0 toutes les valeurs de table
 }
+
