78a140bc260295d26ad7b53d06794b113352e661
Bafouye <cosme.pommier@ecl18.ec-lyon.fr>
2020-03-30 10:26:25 +0200

    Création classe tesselle

---
+++ 2048/tesselle.cpp
@@ -0,0 +1,16 @@
+#include "tesselle.h"
+
+Tesselle::Tesselle(int nombre, int couleur, int i, int j)
+{
+
+}
+
+Tesselle(int nombre, int couleur, int i, int j);
+
+void SetPosition(int i, int j);
+void IncrNb();
+void IncrCoul();
+int GetScore();
+int GetI();
+int GetJ();
+void Fusion(Tesselle &t);
